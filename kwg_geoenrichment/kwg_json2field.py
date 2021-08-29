849bc0ac0e9e8fb9efa0268f9beb0e1cb4f12df9
Rushiraj Nenuji <19696935+rushirajnenuji@users.noreply.github.com>
2021-08-29 13:28:44 -0700

    Adding functionality to add attribute to existing table and create new attribute table
    Adding functionality to add attribute to existing table and create new attribute table

--- kwg_geoenrichment/kwg_json2field.py
+++ kwg_geoenrichment/kwg_json2field.py
@@ -1,5 +1,10 @@
+import json
 from collections import OrderedDict, namedtuple
 from decimal import Decimal
+
+from PyQt5.QtCore import QVariant
+from qgis._core import QgsVectorLayer, QgsMessageLog, Qgis, QgsField, QgsFields, QgsFeature, QgsGeometry, \
+    QgsVectorFileWriter, QgsProject
 
 from .kwg_util import kwg_util
 
@@ -203,7 +208,8 @@ class kwg_json2field:
             keyPropertyList.append(jsonItem[keyPropertyName]["value"])
 
         keyValueDict = dict(zip(keyPropertyList, valuePropertyList))
-        # arcpy.AddMessage("keyValueDict: {0}".format(keyValueDict))
+        # QgsMessageLog.logMessage("keyValueDict: " + json.dumps(keyValueDict),
+        #                          "kwg_geoenrichment", level=Qgis.Info)
         return keyValueDict
 
 
@@ -219,3 +225,128 @@ class kwg_json2field:
             return Decimal(fieldValue)
         elif fieldDataType == "FLOAT":
             return float(fieldValue)
+
+
+    def createMappingTableFromJSON(self, jsonBindingObject, keyPropertyName,
+                                   valuePropertyName, valuePropertyURL,
+                                   keyPropertyFieldName, isInverse, isSubDivisionTable, featureClassName="kwg_results", outputLocation="", ifaceObj=None):
+
+
+        currentValuePropertyName = self.kwgUtil.getPropertyName(valuePropertyURL)
+
+        # currentValuePropertyName = SPARQLUtil.make_prefixed_iri(valuePropertyURL)
+        if isInverse == True:
+            currentValuePropertyName = "is_" + currentValuePropertyName + "_Of"
+        if isSubDivisionTable == True:
+            currentValuePropertyName = "subDivisionIRI"
+        tableName = featureClassName + "_" + keyPropertyFieldName + "_" + currentValuePropertyName
+
+        # TODO:  implement QGIS logic
+        # tablePath = Json2Field.getNoExistTableNameInWorkspace(outputLocation, tableName)
+        valuePropertyFieldType = self.fieldDataTypeDecide(jsonBindingObject, valuePropertyName)
+
+        layerFields = QgsFields()
+        layerFields.append(QgsField(keyPropertyFieldName, QVariant.String))
+        if valuePropertyFieldType == "TEXT":
+            layerFields.append(QgsField(currentValuePropertyName, QVariant.String))
+
+        else:
+            layerFields.append(QgsField(currentValuePropertyName))
+
+        vl = QgsVectorLayer("POLYGON" + "?crs=epsg:4326", tableName, "memory")
+        pr = vl.dataProvider()
+        pr.addAttributes(layerFields)
+        vl.updateFields()
+
+        PropertyValue = namedtuple("PropertyValue", ["key", "value"])
+        propertyValueSet = set()
+        for jsonItem in jsonBindingObject:
+            pair = PropertyValue(key=jsonItem[keyPropertyName]["value"], value=jsonItem[valuePropertyName]["value"])
+            propertyValueSet.add(pair)
+
+        propertyValueList = list(propertyValueSet)
+
+        if outputLocation == None:
+            QgsMessageLog.logMessage("No data will be added to the map document.", level=Qgis.Info)
+        else:
+
+            for item in propertyValueList:
+                feat = QgsFeature()
+
+                # TODO: handle the CRS
+                # feat.setGeometry(self.transformSourceCRStoDestinationCRS(geom, src=4326, dest=3857))
+
+                feat.setAttributes([item.key, item.value])
+                pr.addFeature(feat)
+            vl.updateExtents()
+
+            options = QgsVectorFileWriter.SaveVectorOptions()
+            context = QgsProject.instance().transformContext()
+            error = QgsVectorFileWriter.writeAsVectorFormatV2(vl, outputLocation, context, options)
+            ifaceObj.addVectorLayer(outputLocation, tableName, 'ogr')
+
+        return error[0] == QgsVectorFileWriter.NoError
+
+
+    def addFieldInTableByMapping(self, jsonBindingObject, keyPropertyName, valuePropertyName, keyPropertyFieldName, valuePropertyURL, isInverse, featureClassName="kwg_results", gpkgLocation=""):
+        # according to the json object from sparql query which contains the mapping from keyProperty to valueProperty, add field in the Table
+        # change the field name if there is already a field which has the same name in table
+        # jsonBindingObject: the json object from sparql query which contains the mapping from keyProperty to valueProperty, ex. functionalPropertyJSON
+        # keyPropertyName: the name of keyProperty in JSON object, ex. wikidataSub
+        # valuePropertyName: the name of valueProperty in JSON object, ex. o
+        # keyPropertyFieldName:  the name of the field which stores the value of keyProperty, ex. URL
+        # valuePropertyURL: tatomhe URL of valueProperty, we use it to get the field name of valueProperty, ex. functionalProperty
+        # isInverse: Boolean variable indicates whether the value we get is the subject value or object value of valuePropertyURL
+
+        keyValueDict = self.buildDictFromJSONToModifyTable(jsonBindingObject, keyPropertyName, valuePropertyName)
+
+        currentValuePropertyName = self.kwgUtil.getPropertyName(valuePropertyURL)
+        # currentValuePropertyName = SPARQLUtil.make_prefixed_iri(valuePropertyURL)
+        if isInverse == True:
+            currentValuePropertyName = "is_" + currentValuePropertyName + "_Of"
+
+        gpkg_places_layer = gpkgLocation + "|layername=%s" % (featureClassName)
+
+        vlayer = QgsVectorLayer(gpkg_places_layer, "kwg_results", "ogr")
+
+        if not vlayer.isValid():
+            QgsMessageLog.logMessage("Error reading the table",
+                                     "kwg_geoenrichment", level=Qgis.Warning)
+            return 0
+
+        # TODO: temp set up; move to permanent
+        # currentFieldName = self.kwgUtil.getFieldNameWithTable(currentValuePropertyName, featureClassName, gpkgLocation)
+        currentFieldName =currentValuePropertyName
+        if currentFieldName == -1:
+            # messages.addWarningMessage("The table of current feature class has more than 10 fields for property name {0}.".format(currentValuePropertyName))
+            pass
+        else:
+
+            # add one field for each functional property in input feature class
+            fieldType = self.fieldDataTypeDecide(jsonBindingObject, valuePropertyName)
+            # arcpy.AddMessage("fieldType: {0}".format(fieldType))
+            if fieldType == "TEXT":
+                fieldLength = self.fieldLengthDecide(jsonBindingObject, valuePropertyName)
+                pr = vlayer.dataProvider()
+                pr.addAttributes([QgsField(currentFieldName, QVariant.String)])
+                vlayer.updateFields()
+            else:
+                pr = vlayer.dataProvider()
+                pr.addAttributes([QgsField(currentFieldName)])
+                vlayer.updateFields()
+
+            selected_feature = vlayer.getFeatures()
+            QgsMessageLog.logMessage(
+                "Features selected, beginning editing the values for those features " ,
+                "kwg_geoenrichment", level=Qgis.Info)
+            vlayer.startEditing()
+
+            for feature in selected_feature:
+                currentKeyPropertyValue = feature[keyPropertyFieldName]
+                if currentKeyPropertyValue in keyValueDict:
+                    feature[currentFieldName] = keyValueDict[currentKeyPropertyValue]
+                vlayer.updateFeature(feature)
+            vlayer.commitChanges()
+
+        return 1
+
